53c8b1200d2e0723d9d21e033ddd94f3ee56e45a
ghady alhamad <lady-1987@live.com>
2022-02-28 21:55:19 +0200

    Read Me

--- src/constants/firebase.ts
+++ src/constants/firebase.ts
@@ -4,7 +4,13 @@ import "firebase/compat/firestore";
 import { getFirestore } from "firebase/firestore";
 
 export const firebaseConfig = {
-   /* Your Configuration */
+  apiKey: "AIzaSyA6qfhNOg54A8Evfe0G2PeRS2h4f81Pt3k",
+  authDomain: "oppa-coding-challenge.firebaseapp.com",
+  databaseURL: "https://oppa-coding-challenge.firebaseio.com",
+  projectId: "oppa-coding-challenge",
+  storageBucket: "oppa-coding-challenge.appspot.com",
+  messagingSenderId: "246738910516",
+  appId: "1:246738910516:web:155d7bef48ef1f1132f961",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
